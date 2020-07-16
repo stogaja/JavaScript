3e5ea03ef6ea92693c1b5ffbe1a9b405eb52952d
Steve <ogajasteve76@gmail.com>
2020-07-16 09:44:55 +0300

    backup

---
+++ Chat/client/src/components/Join/Join.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
+
+export default function SignIn() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join a Room</h1>
+        <div>
+          <input
+            placeholder="Chama username..."
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Chama Room name..."
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={"button mt-20"} type="submit">
+            Join Room
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
