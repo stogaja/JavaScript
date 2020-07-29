6346d4394f0d6571b387ad831122409bc5837894
Steve <ogajasteve76@gmail.com>
2020-07-29 11:50:40 +0300

    Multi layout

--- e-commerce/js/function.js
+++ e-commerce/js/function.js
@@ -1,55 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-	// Navigation links hover
-	let navLinks = document.querySelectorAll('nav a, .social a');
+document.addEventListener("DOMContentLoaded", () => {
+  // Navigation links hover
+  let navLinks = document.querySelectorAll("nav a, .social a");
 
-	navLinks.forEach((navLink) => {
-		navLink.addEventListener('mouseenter', () => {
-			let letter = navLink.querySelectorAll('.letter');
-			anime.remove(letter);
-			anime.timeline({
-				targets: letter,
-			})
-			.add({
-				translateX: [0, -30],
-				opacity: [1, 0],
-				easing: 'easeInExpo',
-				duration: 500,
-				delay: (el, i) => 30 * i,
-			})
-			.add({
-				translateX: [40, 0],
-				opacity: [0, 1],
-				easing: 'easeOutExpo',
-				duration: 1100,
-				delay: (el, i) => 30 * i,
-			})
-		})
-	})
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("mouseenter", () => {
+      let letter = navLink.querySelectorAll(".letter");
+      anime.remove(letter);
+      anime
+        .timeline({
+          targets: letter,
+        })
+        .add({
+          translateX: [0, -30],
+          opacity: [1, 0],
+          easing: "easeInExpo",
+          duration: 500,
+          delay: (el, i) => 30 * i,
+        })
+        .add({
+          translateX: [40, 0],
+          opacity: [0, 1],
+          easing: "easeOutExpo",
+          duration: 1100,
+          delay: (el, i) => 30 * i,
+        });
+    });
+  });
 
-	// Gallery nav hover
-	let galleryLinks = document.querySelectorAll('.gallery-nav div');
+  // Gallery nav hover
+  let galleryLinks = document.querySelectorAll(".gallery-nav div");
 
-	galleryLinks.forEach((galleryLink) => {
-		galleryLink.addEventListener('mouseenter', () => {
-			anime.remove(galleryLink);
-			anime({
-				targets: galleryLink,
-				scale: 1.1,
-				translateX: -5,
-				translateY: -5,
-				easing: 'easeOutExpo',
-			})
-		})
+  galleryLinks.forEach((galleryLink) => {
+    galleryLink.addEventListener("mouseenter", () => {
+      anime.remove(galleryLink);
+      anime({
+        targets: galleryLink,
+        scale: 1.1,
+        translateX: -5,
+        translateY: -5,
+        easing: "easeOutExpo",
+      });
+    });
 
-		galleryLink.addEventListener('mouseleave', () => {
-			anime.remove(galleryLink);
-			anime({
-				targets: galleryLink,
-				scale: 1,
-				translateX: 0,
-				translateY: 0,
-				easing: 'easeOutExpo',
-			})
-		})
-	})
-})
+    galleryLink.addEventListener("mouseleave", () => {
+      anime.remove(galleryLink);
+      anime({
+        targets: galleryLink,
+        scale: 1,
+        translateX: 0,
+        translateY: 0,
+        easing: "easeOutExpo",
+      });
+    });
+  });
+});
