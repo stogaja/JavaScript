efdf06e29b95c875923e67871a0da870a57e2130
Steve <ogajasteve76@gmail.com>
2020-08-04 12:01:17 +0300

    react work

---
+++ REACT.js/urembobeautyspot/src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../../node_modules/materialize-css/dist/css/materialize.css';
+import Anchor_ink from 
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
